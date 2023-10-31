ccdefa10d6b66fef9dbde2fa2bd5f34e50dcf6ad
GiyanRa <giyanraditya024@gmail.com>
2023-10-31 11:37:16 +0700

    Commit ISP

---
+++ ISP/Motorcycle.java
@@ -0,0 +1,11 @@
+public class Motorcycle implements VehicleInterface{
+    @Override
+		public void drive() {	
+		}
+		@Override
+		public void stop() {	
+		}
+		@Override
+		public void refuel() {		
+		}
+}
